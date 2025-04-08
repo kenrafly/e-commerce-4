7435aa4527c2e8b923ac56d9ab17e9f4b24fd474
Rafly Ardiansyah <rafly21ardiansyah@gmail.com>
2025-04-08 12:56:16 +0700

    unknown

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -35,7 +35,7 @@ export async function signOutUser() {
 }
 
 // Sign up the user
-export async function signUpUser(prevState: unknwon, formData: FormData) {
+export async function signUpUser(prevState: unknown, formData: FormData) {
   try {
     const user = signUpFormSchema.parse({
       name: formData.get("name"),
